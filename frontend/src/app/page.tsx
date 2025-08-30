e4778cf4ac55520406325250fd56f4044acd03fc
dicethedev <dicethedev@gmail.com>
2025-08-30 22:11:03 +0100

    fix: minikit error resolved

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,14 +2,14 @@
 
 import { HomeView } from "./views";
 import { useEffect } from 'react';
-import { useMiniKit } from '@coinbase/onchainkit/minikit';
+// import { useMiniKit } from '@coinbase/onchainkit/minikit';
 
 export default function HomePage() {
-   const { setFrameReady, isFrameReady } = useMiniKit();
+  //  const { setFrameReady, isFrameReady } = useMiniKit();
 
-  useEffect(() => {
-    if (!isFrameReady) setFrameReady();
-  }, [isFrameReady, setFrameReady]);
+  // useEffect(() => {
+  //   if (!isFrameReady) setFrameReady();
+  // }, [isFrameReady, setFrameReady]);
 
   return <HomeView />;
 }
